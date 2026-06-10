8635050140fb7424d01a7c29db2e8bf3e2f81d51
Gabriella <gabriella.acproni@gmail.com>
2026-06-10 23:36:37 +0100

    Projeto acadêmico inicial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+        System.out.println("=== Sistema Acadêmico ===");
+        System.out.println("Bem-vindo ao sistema!");
+        System.out.println("Projeto configurado com sucesso!");
+        
+        // TODO: Implementar menu principal aqui
+        // Por enquanto, apenas um teste de funcionamento
+        System.out.println("\nSistema funcionando com JDK 17!");
+    }
+}
